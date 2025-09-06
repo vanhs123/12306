138bf6316384fbb8f2c91b6f351d3f357064adad
hs van <vanhs123@outlook.com>
2025-09-06 08:57:44 +0800

    新增member会员模块

---
+++ member/src/main/java/com/van/train/member/config/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.van.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.van")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
