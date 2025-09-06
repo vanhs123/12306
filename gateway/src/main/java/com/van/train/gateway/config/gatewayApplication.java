354828f375b92b3afbd70f3721f726459dfac317
hs van <vanhs123@outlook.com>
2025-09-06 16:20:37 +0800

    4.7增加网关模块

---
+++ gateway/src/main/java/com/van/train/gateway/config/gatewayApplication.java
@@ -0,0 +1,26 @@
+package com.van.train.gateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.van")
+public class gatewayApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(gatewayApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app =new SpringApplication(gatewayApplication.class);
+
+        Environment env = app.run(args).getEnvironment();
+
+        LOG.info("启动成功！！");
+
+        LOG.info("网关地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
